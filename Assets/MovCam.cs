93f59095d48a832f335b1da0546b9811cb61385c
Chris Papenfuß <Chris.papenfuss@data-experts.de>
2015-03-31 09:46:31 +0200

    First Commit

---
+++ Assets/MovCam.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovCam : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        startVec = Input.acceleration;
+        camDist = Vector3.Distance(Vector3.zero, gameObject.transform.position);
+	}
+
+    float camDist;
+    Vector3 startVec;
+    Quaternion camRot = Quaternion.AngleAxis(90, new Vector3(1, 0, 0));
+	
+	// Update is called once per frame
+	void Update () {
+        var acc = new Vector3(Input.acceleration.x, - Input.acceleration.z, Input.acceleration.y);
+        acc.Normalize();
+        gameObject.transform.position = Vector3.zero + (acc * camDist);    
+        gameObject.transform.LookAt(GameObject.FindGameObjectWithTag("Player").transform.position, camRot*acc);
+                
+        GameObject.FindGameObjectWithTag("Debug").GetComponent<UnityEngine.UI.Text>().text = acc.ToString()+ "\r\n"+  (camRot * acc).ToString();
+	}
+}
---
+++ Assets/dummy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dummy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        collider = GetComponent<TerrainCollider>();
+        terrData = collider.terrainData;
+	}
+    TerrainCollider collider;
+    TerrainData terrData;
+	
+	// Update is called once per frame
+	void Update () {
+        float[,] data = new float[terrData.heightmapHeight, terrData.heightmapWidth];
+        for (int x = 0; x < terrData.heightmapHeight; x++)
+        {
+            for (int y = 0; y < terrData.heightmapWidth; y++)
+            {
+                data[x, y] = Random.Range(0f,0.03f);
+            }
+        }
+        terrData.SetHeights(0, 0, data);
+	}
+}
